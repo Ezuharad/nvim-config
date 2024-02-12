1760b6566cf134309348f2c7b9bb4b2f27690fd4
Steven Chiacchira <schiacchira@Gmail.com>
2024-02-11 23:46:15 -0500

    Added todo, fixed python debugger

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -6,9 +6,9 @@ return {
     end
   },
   {
-    'mfussenegger/nvim-dap',
+    'rcarriga/nvim-dap-ui',
     dependencies = {
-      'rcarriga/nvim-dap-ui'
+      'mfussenegger/nvim-dap'
     },
     config = function()
       local dap, dapui = require("dap"), require("dapui")
@@ -25,8 +25,27 @@ return {
         dapui.close()
       end
 
+      dapui.setup()
+
+      -- set keybinds
       vim.keymap.set('n', '<leader>dt', dap.toggle_breakpoint, {})
       vim.keymap.set('n', '<leader>dc', dap.continue, {})
+      vim.keymap.set('n', '<leader>dd', dap.step_over, {})
+      vim.keymap.set('n', '<leader>dj', dap.step_into, {})
+      vim.keymap.set('n', '<leader>dk', dap.step_out, {})
+      vim.keymap.set('n', '<leader>dh', dap.step_back, {}) -- TODO: check if supported
     end
   },
+  {
+    'folke/neodev.nvim',
+    opts = {},
+    config = function()
+      require("neodev").setup({
+        library = {
+          plugins = { "nvim-dap-ui" },
+          types = true
+        },
+      })
+    end
+  }
 }
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,10 +2,14 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   config = function()
-    require('toggleterm').setup({
-      open_mapping = '<leader>tt',
+    local toggleterm = require('toggleterm')
+    toggleterm.setup({
+      -- open_mapping = '<leader>tt', -- TODO why does this operate in normal mode?
+      insert_mapping = false,
       shell = 'zsh'
     })
+
+  vim.keymap.set('n', '<leader>tt', ':ToggleTerm<CR>', {silent = true})
   end
 }
 
